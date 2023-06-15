dc9ba5f0b5078bb8b8ae5ef86bed4b22d372144a
omardepaiva <omar.paiva@gmail.com>
2023-06-15 08:41:22 -0300

    feat: ajustes finais

--- src/controllers/pessoa.ts
+++ src/controllers/pessoa.ts
@@ -3,6 +3,7 @@ import { Request, Response } from "express";
 
 const prisma = new PrismaClient();
 
+// CREATE - CRIAÇÃO DE PESSOA
 export async function create(req: Request, res: Response) {
   const pessoa = await prisma.pessoa.create({
     data: req.body,
@@ -10,13 +11,14 @@ export async function create(req: Request, res: Response) {
 
   return res.json(pessoa);
 }
-
+// READ - LEITURA DE UM ÚNICO ID
 export async function read(req: Request, res: Response) {
   const { id } = req.params;
   const data = await prisma.pessoa.findFirst({ where: { id: Number(id) } });
   return res.send(data);
 }
 
+// UPDATE - atualização de pessoa
 export async function update(req: Request, res: Response) {
   const { id } = req.params;
   const updatedData = await prisma.pessoa.update({
@@ -26,8 +28,16 @@ export async function update(req: Request, res: Response) {
   return res.send(updatedData);
 }
 
-// export async function delete(req: Request, res: Response) {}
+//deleteId = DELETAR UM ID
+export async function deleteId(req: Request, res: Response) {
+  const { id } = req.params;
+  const deleteId = await prisma.pessoa.delete({
+    where: { id: Number(id) },
+  });
+  return res.send(deleteId);
+}
 
+// ALL - ENCONTRAR TODOS OS IDS
 export async function all(req: Request, res: Response) {
   const data = await prisma.pessoa.findMany();
   return res.send(data);
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import { all, create, read, update } from "./controllers/pessoa";
+import { all, create, deleteId, read, update } from "./controllers/pessoa";
 import cors from "cors"; // importando biblioteca do cors
 import express from "express";
 const app = express();
@@ -9,7 +9,9 @@ app.use(cors()); //declarando o uso do cors
 app.listen(port, () => {
   console.log(`Server ligado ${port}`);
 });
+// REQUISIÇÕES HTTP
 app.get("/pessoas", all);
 app.get("/pessoa/:id", read);
 app.post("/pessoa", create);
 app.put("/pessoa/:id", update);
+app.delete("/pessoa/:id", deleteId);
